8a8364fe0ba37e3373ac7f6a646c2d5b5eb9ac55
Amin <m.a.naghiyan02@gmail.com>
2021-06-03 12:52:14 +0430

    Design the initial appearance of page Show all

--- build-Project-Desktop_Qt_5_12_10_MinGW_32_bit-Debug/ui_show_all.h
+++ build-Project-Desktop_Qt_5_12_10_MinGW_32_bit-Debug/ui_show_all.h
@@ -10,6 +10,7 @@
 #define UI_SHOW_ALL_H
 
 #include <QtCore/QVariant>
+#include <QtGui/QIcon>
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QHBoxLayout>
 #include <QtWidgets/QPushButton>
@@ -27,7 +28,12 @@ public:
     QVBoxLayout *verticalLayout_2;
     QHBoxLayout *horizontalLayout;
     QSpacerItem *horizontalSpacer;
-    QPushButton *pushButton;
+    QPushButton *close;
+    QSpacerItem *verticalSpacer_3;
+    QHBoxLayout *horizontalLayout_4;
+    QSpacerItem *horizontalSpacer_5;
+    QTextBrowser *textBrowser_2;
+    QSpacerItem *horizontalSpacer_6;
     QSpacerItem *verticalSpacer_2;
     QHBoxLayout *horizontalLayout_2;
     QSpacerItem *horizontalSpacer_2;
@@ -35,17 +41,17 @@ public:
     QSpacerItem *horizontalSpacer_3;
     QSpacerItem *verticalSpacer;
     QHBoxLayout *horizontalLayout_3;
-    QPushButton *pushButton_2;
+    QPushButton *back;
     QSpacerItem *horizontalSpacer_4;
 
     void setupUi(QWidget *show_all)
     {
         if (show_all->objectName().isEmpty())
             show_all->setObjectName(QString::fromUtf8("show_all"));
-        show_all->resize(800, 600);
+        show_all->resize(800, 700);
         verticalLayout = new QVBoxLayout(show_all);
         verticalLayout->setObjectName(QString::fromUtf8("verticalLayout"));
-        verticalLayout->setContentsMargins(-1, 0, -1, -1);
+        verticalLayout->setContentsMargins(5, 0, 5, 5);
         verticalLayout_2 = new QVBoxLayout();
         verticalLayout_2->setObjectName(QString::fromUtf8("verticalLayout_2"));
         verticalLayout_2->setContentsMargins(-1, -1, -1, 0);
@@ -56,21 +62,55 @@ public:
 
         horizontalLayout->addItem(horizontalSpacer);
 
-        pushButton = new QPushButton(show_all);
-        pushButton->setObjectName(QString::fromUtf8("pushButton"));
+        close = new QPushButton(show_all);
+        close->setObjectName(QString::fromUtf8("close"));
         QSizePolicy sizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
         sizePolicy.setHorizontalStretch(60);
         sizePolicy.setVerticalStretch(40);
-        sizePolicy.setHeightForWidth(pushButton->sizePolicy().hasHeightForWidth());
-        pushButton->setSizePolicy(sizePolicy);
-        pushButton->setMinimumSize(QSize(60, 40));
+        sizePolicy.setHeightForWidth(close->sizePolicy().hasHeightForWidth());
+        close->setSizePolicy(sizePolicy);
+        close->setMinimumSize(QSize(60, 40));
+        QIcon icon;
+        icon.addFile(QString::fromUtf8(":/icons/close.png"), QSize(), QIcon::Normal, QIcon::Off);
+        close->setIcon(icon);
+        close->setIconSize(QSize(60, 40));
 
-        horizontalLayout->addWidget(pushButton);
+        horizontalLayout->addWidget(close);
 
 
         verticalLayout_2->addLayout(horizontalLayout);
 
-        verticalSpacer_2 = new QSpacerItem(20, 40, QSizePolicy::Minimum, QSizePolicy::Fixed);
+        verticalSpacer_3 = new QSpacerItem(20, 30, QSizePolicy::Minimum, QSizePolicy::Fixed);
+
+        verticalLayout_2->addItem(verticalSpacer_3);
+
+        horizontalLayout_4 = new QHBoxLayout();
+        horizontalLayout_4->setObjectName(QString::fromUtf8("horizontalLayout_4"));
+        horizontalLayout_4->setContentsMargins(-1, 0, -1, -1);
+        horizontalSpacer_5 = new QSpacerItem(130, 20, QSizePolicy::Fixed, QSizePolicy::Minimum);
+
+        horizontalLayout_4->addItem(horizontalSpacer_5);
+
+        textBrowser_2 = new QTextBrowser(show_all);
+        textBrowser_2->setObjectName(QString::fromUtf8("textBrowser_2"));
+        QSizePolicy sizePolicy1(QSizePolicy::Fixed, QSizePolicy::Fixed);
+        sizePolicy1.setHorizontalStretch(0);
+        sizePolicy1.setVerticalStretch(0);
+        sizePolicy1.setHeightForWidth(textBrowser_2->sizePolicy().hasHeightForWidth());
+        textBrowser_2->setSizePolicy(sizePolicy1);
+        textBrowser_2->setMinimumSize(QSize(509, 50));
+        textBrowser_2->setMaximumSize(QSize(16777215, 50));
+
+        horizontalLayout_4->addWidget(textBrowser_2);
+
+        horizontalSpacer_6 = new QSpacerItem(130, 20, QSizePolicy::Fixed, QSizePolicy::Minimum);
+
+        horizontalLayout_4->addItem(horizontalSpacer_6);
+
+
+        verticalLayout_2->addLayout(horizontalLayout_4);
+
+        verticalSpacer_2 = new QSpacerItem(20, 30, QSizePolicy::Minimum, QSizePolicy::Fixed);
 
         verticalLayout_2->addItem(verticalSpacer_2);
 
@@ -93,23 +133,24 @@ public:
 
         verticalLayout_2->addLayout(horizontalLayout_2);
 
-        verticalSpacer = new QSpacerItem(20, 40, QSizePolicy::Minimum, QSizePolicy::Fixed);
+        verticalSpacer = new QSpacerItem(20, 30, QSizePolicy::Minimum, QSizePolicy::Fixed);
 
         verticalLayout_2->addItem(verticalSpacer);
 
         horizontalLayout_3 = new QHBoxLayout();
         horizontalLayout_3->setObjectName(QString::fromUtf8("horizontalLayout_3"));
         horizontalLayout_3->setContentsMargins(-1, 0, -1, -1);
-        pushButton_2 = new QPushButton(show_all);
-        pushButton_2->setObjectName(QString::fromUtf8("pushButton_2"));
-        QSizePolicy sizePolicy1(QSizePolicy::Fixed, QSizePolicy::Fixed);
-        sizePolicy1.setHorizontalStretch(0);
-        sizePolicy1.setVerticalStretch(0);
-        sizePolicy1.setHeightForWidth(pushButton_2->sizePolicy().hasHeightForWidth());
-        pushButton_2->setSizePolicy(sizePolicy1);
-        pushButton_2->setMinimumSize(QSize(60, 40));
+        back = new QPushButton(show_all);
+        back->setObjectName(QString::fromUtf8("back"));
+        sizePolicy1.setHeightForWidth(back->sizePolicy().hasHeightForWidth());
+        back->setSizePolicy(sizePolicy1);
+        back->setMinimumSize(QSize(60, 40));
+        QIcon icon1;
+        icon1.addFile(QString::fromUtf8(":/icons/back.png"), QSize(), QIcon::Normal, QIcon::Off);
+        back->setIcon(icon1);
+        back->setIconSize(QSize(60, 40));
 
-        horizontalLayout_3->addWidget(pushButton_2);
+        horizontalLayout_3->addWidget(back);
 
         horizontalSpacer_4 = new QSpacerItem(40, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
 
@@ -130,13 +171,18 @@ public:
     void retranslateUi(QWidget *show_all)
     {
         show_all->setWindowTitle(QApplication::translate("show_all", "Form", nullptr));
-        pushButton->setText(QString());
+        close->setText(QString());
+        textBrowser_2->setHtml(QApplication::translate("show_all", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:'MS Shell Dlg 2'; font-size:8.05594pt; font-weight:400; font-style:normal;\">\n"
+"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:14pt;\">There are all of the books in the libtary:</span></p></body></html>", nullptr));
         textBrowser->setHtml(QApplication::translate("show_all", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
 "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
 "p, li { white-space: pre-wrap; }\n"
 "</style></head><body style=\" font-family:'MS Shell Dlg 2'; font-size:8.05594pt; font-weight:400; font-style:normal;\">\n"
 "<p align=\"center\" style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:8.05594pt;\"><br /></p></body></html>", nullptr));
-        pushButton_2->setText(QString());
+        back->setText(QString());
     } // retranslateUi
 
 };
